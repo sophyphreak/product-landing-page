61d3ee3c5a0fd6a21cc358caa7b845508fcfdc3e
Andrew <arhorn@smcm.edu>
2019-01-30 16:28:29 +0700

    added pricing to app

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import Navigation from './Navigation/Navigation';
 import EmailForm from './EmailForm/EmailForm';
 import Features from './Features/Features';
 import Video from './Video/Video';
+import Pricing from './Pricing/Pricing';
 
 const App = () => (
   <div>
@@ -10,6 +11,7 @@ const App = () => (
     <EmailForm />
     <Features />
     <Video />
+    <Pricing />
   </div>
 );
 
