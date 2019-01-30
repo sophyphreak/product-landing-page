12249369e7e638980636ed6276fb43b2da8d11e5
Andrew <arhorn@smcm.edu>
2019-01-30 15:47:19 +0700

    added features

---
+++ src/components/Features/Features.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Col, Row } from 'reactstrap';
+import { sectionStyle } from './FeaturesStyle';
+
+const Features = () => (
+  <section id="features" style={sectionStyle} >
+  <Row>
+    <Col sm={{ size: 6, offset: 3}} xs="12" >
+    <h2>features:</h2>
+    <ul>
+    <li>2500-year-old book</li>
+    <li>read crazy stories about mythical birds, animals, and people</li>
+    <li>see confucius say crazy things</li>
+    <li>learn stuff!</li>
+    </ul>
+    
+    </Col>
+  </Row>
+  </section>
+);
+
+export default Features;
---
+++ src/components/Features/FeaturesStyle.js
@@ -0,0 +1,3 @@
+export const sectionStyle = {
+  marginTop: '5em'
+}
