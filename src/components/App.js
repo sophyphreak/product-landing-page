e362ba7c396c2316c11acfca40891bb78aaf44c0
Andrew <arhorn@smcm.edu>
2019-01-30 15:47:58 +0700

    added features and vide to app

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import Navigation from './Navigation/Navigation';
 import EmailForm from './EmailForm/EmailForm';
+import Features from './Features/Features';
+import Video from './Video/Video';
 
 const App = () => (
   <div>
     <Navigation />
     <EmailForm />
+    <Features />
+    <Video />
   </div>
 );
 
