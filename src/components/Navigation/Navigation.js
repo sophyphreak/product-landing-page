3af85dfb5f66f450a5dd4a8e8fc5d6d325a51824
Andrew <arhorn@smcm.edu>
2019-01-30 14:27:39 +0700

    added logo

---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink
+} from 'reactstrap';
+import Logo from '../Logo';
+
+export default class Navigation extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      isOpen: false
+    };
+  }
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
+  render() {
+    return (
+      <div>
+        <Navbar color="light" light expand="md" fixed="top">
+          <NavbarBrand href="/"><Logo />zhuangzi</NavbarBrand>
+          <NavbarToggler onClick={this.toggle} />
+          <Collapse isOpen={this.state.isOpen} navbar>
+            <Nav className="ml-auto" navbar>
+              <NavItem>
+                <NavLink href="#features">features</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="#how-it-works">
+                  how it works
+                </NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="#pricing">pricing</NavLink>
+              </NavItem>
+            </Nav>
+          </Collapse>
+        </Navbar>
+      </div>
+    );
+  }
+}
